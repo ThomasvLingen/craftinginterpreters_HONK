ab761b4e58867616bb5b5e5785398e2fc4c50786
Thomas van Lingen <thomasvlingen@gmail.com>
2018-03-14 00:04:33 +0100

    fix: remove debug print

--- src/Evaluator/DiagnosticsTokenTracker.cpp
+++ src/Evaluator/DiagnosticsTokenTracker.cpp
@@ -2,7 +2,6 @@
 // Created by mafn on 3/13/18.
 //
 
-#include <iostream>
 #include "DiagnosticsTokenTracker.hpp"
 
 namespace Honk
@@ -23,13 +22,11 @@ namespace Honk
 
     void DiagnosticsTokenTracker::scope_exit()
     {
-        std::cout << "POP" << std::endl;
         this->_callstack.pop();
     }
 
     void DiagnosticsTokenTracker::add_call(const Token* call)
     {
-        std::cout << "PUSH" << std::endl;
         this->_callstack.push(call);
     }
 }
